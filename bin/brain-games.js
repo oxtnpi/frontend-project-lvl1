8c4c90647a3f7d8ffc9b5ba655d36ce5ff12f44d
Anastasiya Turenko <oxtnpi@gmail.com>
2021-05-12 18:36:31 +0600

    add cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+
+import { welcome } from "../src/cli.js";
+import { askName } from "../src/cli.js";
+import { name } from "../src/cli.js";
+welcome;
+name;
+askName;
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const welcome = console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+const askName = console.log('Hello, ' + name + "!");
+
+export {welcome, name, askName};
